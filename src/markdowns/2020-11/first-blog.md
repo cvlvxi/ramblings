2a5e2e54394c25b95181f4f17a1070beedb6ea08
cvlvxi <cvlvxi@outlook.com>
2020-11-02 11:04:10 +1100

    update

---
+++ src/markdowns/2020-11/first-blog.md
@@ -0,0 +1,20 @@
+# This is a blog
+
+```c++
+#include <iostream>
+
+int main(void) {
+  return 0
+}
+```
+
+| Table | One |
+| - | - | 
+|dog | cat |
+
+## Hello
+- list item
+- bleh
+
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,34 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import LandingPage from "../components/LandingPage";
-import LandingPage2 from "../components/LandingPage2";
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../components/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(Router)
 
-export const routes = [
-  {
-    path: "/",
-    name: "LandingPage",
-    component: LandingPage,
-  },
-  {
-    path: "/dog",
-    name: "LandingPage2",
-    component: LandingPage2
-  },
+import BlogEntries from '../static/blogs.json';
 
-];
+const blogRoutes = Object.keys(BlogEntries).map(section => {
+  const children = BlogEntries[section].map(child => ({
+    path: child.id,
+    name: child.id,
+    component: () => import(`../markdowns/${section}/${child.id}.md`)
+  }))
+  return {
+    path: `/${section}`,
+    name: section,
+    component: () => import('../components/Blog.vue'),
+    children
+  }
+})
 
-const router = new VueRouter({
-  routes,
-});
-
-export default router;
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    ...blogRoutes
+  ]
+})
