00f6ac5b77296f215310d9d25cd3e00e48b26e6b
cvlvxi <cvlvxi@outlook.com>
2020-11-02 12:37:14 +1100

    update

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   publicPath: ".",
   outputDir: "docs",
-  configureWebpack: {
-    optimization: {
-      splitChunks: false
-    }
-  },
+  // configureWebpack: {
+  //   optimization: {
+  //     splitChunks: false
+  //   }
+  // },
   chainWebpack(config){
+    config.optimization.delete('splitChunks')
     config.module.rule('md')
       .test(/\.md/)
       .use('vue-loader')
