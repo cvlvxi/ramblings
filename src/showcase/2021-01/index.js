5d1ec75bf0e8c8ba13148d942ae47f7a2272dc6f
cvlvxi <cvlvxi@outlook.com>
2021-01-11 18:29:25 +1100

    update

--- src/showcase/2021-01/index.js
+++ src/showcase/2021-01/index.js
@@ -1,6 +1,15 @@
 export let ShowCase_2021_01 = {
   "2021-01": [
     {
+      "id": "lumagl/luma_gl_test3",
+      "date": "2021 01 11",
+      "title": "Luma GL Sphere Instance",
+      "description": "Testing Sphere",
+      "tags": ["gfx"],
+      "component": import("./lumagl/LumaGLTest3"),
+      "props": false,
+    },
+    {
       "id": "lumagl/luma_gl_test2",
       "date": "2021 01 11",
       "title": "Luma GL Cube Instance",
---
+++ src/showcase/2021-01/lumagl/LumaGLTest3.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <canvas id="helloworld"></canvas>
+  </div>
+</template>
+
+<script>
+import AppAnimationLoop from "./app3.js";
+import { instrumentGLContext } from "@luma.gl/gltools";
+export default {
+  async mounted() {
+    const canvas = document.getElementById("helloworld");
+    let gl = instrumentGLContext(canvas.getContext("webgl"));
+    this.animationLoop = new AppAnimationLoop({ gl: gl });
+    this.animationLoop.start({ canvas: "helloworld" });
+  },
+  destroyed() {
+    this.animationLoop.stop();
+  },
+  data() {
+    return {
+      animationLoop: null
+    };
+  }
+};
+</script>
+
+<style scoped>
+#helloworld {
+  height: 100%;
+  width: 100%;
+}
+</style>
